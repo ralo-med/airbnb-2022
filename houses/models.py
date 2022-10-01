38ef41047d74a2bb2a0b5d44d31c150dcb067ba9
ralo <leona8464@gmail.com>
2022-10-02 00:53:15 +0900

    #4.1 Migrations

--- houses/models.py
+++ houses/models.py
@@ -1,3 +1,16 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+class House(models.Model):
+
+    """Model Definition for House"""
+
+    name = models.CharField(max_length=140)
+    price = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(max_length=140)
+
+    def __str__(self):
+        return self.name

d5d6da2e56b07afd95e030daea5878262bc14a7d
ralo <leona8464@gmail.com>
2022-10-03 15:42:30 +0900

    #5.1 Custom Model

--- houses/admin.py
+++ houses/admin.py
@@ -4,10 +4,6 @@ from .models import House
 # Register your models here.
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    
-    list_display = (
-        "name", "price","address", "pet_friendly"
-    )
-    list_filter = ("price","pet_friendly")
-    search_fields= ("address",)
-    list_editable= ("pet_friendly",)
+
+    list_display = ("name", "price", "address", "pet_friendly")
+    list_filter = ("price", "pet_friendly")

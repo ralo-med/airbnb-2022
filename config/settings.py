cafae63fdc5f06b00cfb93ae94d6bdd3561ebe04
ralo <leona8464@gmail.com>
2023-02-09 21:01:19 +0900

    #10.1 JsonResponse

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+THIRD_PARTY_APPS = [
+    "rest_framework",
+]
 # Application definition
 CUSTOM_APPS = [
     "common.apps.CommonConfig",
@@ -51,7 +53,7 @@ SYSTEM_APPS = [
     "django.contrib.staticfiles",
 ]
 
-INSTALLED_APPS = SYSTEM_APPS + CUSTOM_APPS
+INSTALLED_APPS = SYSTEM_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",

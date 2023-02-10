dab4a4120cc84935f6123fc3f737c0638aff5832
ralo <leona8464@gmail.com>
2023-02-10 16:14:25 +0900

    #10.8 DELETE

--- categories/views.py
+++ categories/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
 from .models import Category
 from .serializers import CategorySerializer
+from rest_framework.status import HTTP_204_NO_CONTENT
 
 
 @api_view(["GET", "POST"])
@@ -23,7 +24,7 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view(["GET", "PUT"])
+@api_view(["GET", "PUT", "DELETE"])
 def category(request, pk):
     try:
         category = Category.objects.get(pk=pk)
@@ -50,3 +51,7 @@ def category(request, pk):
             return Response(CategorySerializer(updated_category).data)
         else:
             return Response(serializer.errors)
+
+    elif request.method == "DELETE":
+        category.delete()
+        return Response(status=HTTP_204_NO_CONTENT)

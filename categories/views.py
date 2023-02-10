dff07f1eca07fa1af87b0e9df0bf29ed76906033
ralo <leona8464@gmail.com>
2023-02-10 16:04:30 +0900

    #10.7 update(

--- categories/views.py
+++ categories/views.py
@@ -1,5 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.exceptions import NotFound
 from .models import Category
 from .serializers import CategorySerializer
 
@@ -22,8 +23,30 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view()
+@api_view(["GET", "PUT"])
 def category(request, pk):
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
+
+    # 먼저 category를 가져오고  (pk는 url에서 온거다)
+
+    if request.method == "GET":
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    # PUT 은 (데이터베이스와, 유저 데이터를 둘다 사용한다)
+    # partial은 데이터가 일부분만 들어와도 ㄱㅊ, 걍 데이터베이스를 사용함
+    # save가 알아서 create쓸지, update쓸지 정해줌
+    elif request.method == "PUT":
+        serializer = CategorySerializer(
+            category,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_category = serializer.save()
+            return Response(CategorySerializer(updated_category).data)
+        else:
+            return Response(serializer.errors)

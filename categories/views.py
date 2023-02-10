e9db11d70400a7e0549f7638cf2b4507401f432d
ralo <leona8464@gmail.com>
2023-02-10 21:41:09 +0900

    #10.10 APIView

--- categories/views.py
+++ categories/views.py
@@ -4,15 +4,16 @@ from rest_framework.exceptions import NotFound
 from .models import Category
 from .serializers import CategorySerializer
 from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework.views import APIView
 
 
-@api_view(["GET", "POST"])
-def categories(request):
-    if request.method == "GET":
+class Categories(APIView):
+    def get(self, request):
         all_categories = Category.objects.all()
         serializer = CategorySerializer(all_categories, many=True)
         return Response(serializer.data)
-    elif request.method == "POST":
+
+    def post(self, request):
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
             new_category = serializer.save()
@@ -24,25 +25,20 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def category(request, pk):
-    try:
-        category = Category.objects.get(pk=pk)
-    except Category.DoesNotExist:
-        raise NotFound
+class CategoryDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise NotFound
 
-    # 먼저 category를 가져오고  (pk는 url에서 온거다)
-
-    if request.method == "GET":
-        serializer = CategorySerializer(category)
+    def get(self, request, pk):
+        serializer = CategorySerializer(self.get_object(pk))
         return Response(serializer.data)
 
-    # PUT 은 (데이터베이스와, 유저 데이터를 둘다 사용한다)
-    # partial은 데이터가 일부분만 들어와도 ㄱㅊ, 걍 데이터베이스를 사용함
-    # save가 알아서 create쓸지, update쓸지 정해줌
-    elif request.method == "PUT":
+    def put(self, request, pk):
         serializer = CategorySerializer(
-            category,
+            self.get_object(pk),
             data=request.data,
             partial=True,
         )
@@ -52,6 +48,6 @@ def category(request, pk):
         else:
             return Response(serializer.errors)
 
-    elif request.method == "DELETE":
-        category.delete()
+    def delete(self, request, pk):
+        self.get_object(pk).delete()
         return Response(status=HTTP_204_NO_CONTENT)

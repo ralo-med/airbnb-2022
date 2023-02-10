0291221c8c3024bcb363d5c0564452dcea4826a8
ralo <leona8464@gmail.com>
2023-02-10 23:43:09 +0900

    #10.12 ModelViewSet

--- categories/views.py
+++ categories/views.py
@@ -1,53 +1,9 @@
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.exceptions import NotFound
 from .models import Category
 from .serializers import CategorySerializer
 from rest_framework.status import HTTP_204_NO_CONTENT
-from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
 
-class Categories(APIView):
-    def get(self, request):
-        all_categories = Category.objects.all()
-        serializer = CategorySerializer(all_categories, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            new_category = serializer.save()
-            # Python 객체를 받았고 이를 JSON으로 다시 받고 response로 보내주는 것이다!
-            return Response(
-                CategorySerializer(new_category).data,
-            )
-        else:
-            return Response(serializer.errors)
-
-
-class CategoryDetail(APIView):
-    def get_object(self, pk):
-        try:
-            return Category.objects.get(pk=pk)
-        except Category.DoesNotExist:
-            raise NotFound
-
-    def get(self, request, pk):
-        serializer = CategorySerializer(self.get_object(pk))
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        serializer = CategorySerializer(
-            self.get_object(pk),
-            data=request.data,
-            partial=True,
-        )
-        if serializer.is_valid():
-            updated_category = serializer.save()
-            return Response(CategorySerializer(updated_category).data)
-        else:
-            return Response(serializer.errors)
-
-    def delete(self, request, pk):
-        self.get_object(pk).delete()
-        return Response(status=HTTP_204_NO_CONTENT)
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
